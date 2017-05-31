9a019e7e348c0d462590be5c3a2bef3e724f9728
Antoine van der Lee <ajvanderlee@gmail.com>
2017-05-31 14:07:15 +0200

    Removed header description

--- Source/Firebase.h
+++ Source/Firebase.h
@@ -1,11 +1,3 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for Firebase.
-FOUNDATION_EXPORT double FirebaseVersionNumber;
-
-//! Project version string for Firebase.
-FOUNDATION_EXPORT const unsigned char FirebaseVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <Firebase/PublicHeader.h>
 #import <FirebaseAnalytics/FirebaseAnalytics.h>
 #import <FirebaseCore/FirebaseCore.h>
